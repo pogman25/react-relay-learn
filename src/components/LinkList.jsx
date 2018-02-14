d0e189f45ffb5e55ab439ddcb7f7609786769c4a
Alexandr Pogorelov <apogorelov@smedialink.com>
2018-02-14 19:36:53 +0300

    next chapter

--- src/components/LinkList.jsx
+++ src/components/LinkList.jsx
@@ -18,7 +18,7 @@ class LinkList extends Component {
 
 export default createFragmentContainer(LinkList, graphql`
     fragment LinkList_viewer on Viewer {
-        allLinks(last: 100, orderBy: createdAt_DESC) @connection(key: "LinkList_allLinks", filter: []) {
+        allLinks(last: 100, orderBy: createdAt_DESC) @connection(key: "LinkList_allLinks", filters: []) {
             edges {
                 node {
                     ...Link_link
---
+++ src/components/__generated__/LinkList_viewer.graphql.js
@@ -0,0 +1,119 @@
+/**
+ * @flow
+ */
+
+/* eslint-disable */
+
+'use strict';
+
+/*::
+import type {ConcreteFragment} from 'relay-runtime';
+export type LinkList_viewer = {|
+  +allLinks: ?{|
+    +edges: ?$ReadOnlyArray<?{|
+      +node: ?{| |};
+    |}>;
+  |};
+|};
+*/
+
+
+const fragment /*: ConcreteFragment*/ = {
+  "argumentDefinitions": [],
+  "kind": "Fragment",
+  "metadata": {
+    "connection": [
+      {
+        "count": null,
+        "cursor": null,
+        "direction": "backward",
+        "path": [
+          "allLinks"
+        ]
+      }
+    ]
+  },
+  "name": "LinkList_viewer",
+  "selections": [
+    {
+      "kind": "LinkedField",
+      "alias": "allLinks",
+      "args": null,
+      "concreteType": "LinkConnection",
+      "name": "__LinkList_allLinks_connection",
+      "plural": false,
+      "selections": [
+        {
+          "kind": "LinkedField",
+          "alias": null,
+          "args": null,
+          "concreteType": "LinkEdge",
+          "name": "edges",
+          "plural": true,
+          "selections": [
+            {
+              "kind": "LinkedField",
+              "alias": null,
+              "args": null,
+              "concreteType": "Link",
+              "name": "node",
+              "plural": false,
+              "selections": [
+                {
+                  "kind": "FragmentSpread",
+                  "name": "Link_link",
+                  "args": null
+                },
+                {
+                  "kind": "ScalarField",
+                  "alias": null,
+                  "args": null,
+                  "name": "__typename",
+                  "storageKey": null
+                }
+              ],
+              "storageKey": null
+            },
+            {
+              "kind": "ScalarField",
+              "alias": null,
+              "args": null,
+              "name": "cursor",
+              "storageKey": null
+            }
+          ],
+          "storageKey": null
+        },
+        {
+          "kind": "LinkedField",
+          "alias": null,
+          "args": null,
+          "concreteType": "PageInfo",
+          "name": "pageInfo",
+          "plural": false,
+          "selections": [
+            {
+              "kind": "ScalarField",
+              "alias": null,
+              "args": null,
+              "name": "hasPreviousPage",
+              "storageKey": null
+            },
+            {
+              "kind": "ScalarField",
+              "alias": null,
+              "args": null,
+              "name": "startCursor",
+              "storageKey": null
+            }
+          ],
+          "storageKey": null
+        }
+      ],
+      "storageKey": null
+    }
+  ],
+  "type": "Viewer"
+};
+
+module.exports = fragment;
